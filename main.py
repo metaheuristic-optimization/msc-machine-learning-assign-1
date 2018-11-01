42dc7b1a84488634aaf59fc17611e88fbcc8f244
el-davo <david.ahern@emc.com>
2018-11-01 13:57:46 +0000

    Updated

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from src.knn import KNN
 
 def main():
-    k = 2
-    knn = KNN('datasets/test.csv', 'datasets/testData2.csv', k)
+    k = 20
+    knn = KNN('datasets/trainingData2.csv', 'datasets/testData2.csv', k)
     knn.run()
-
 
 if __name__ == "__main__":
     main()
--- src/knn.py
+++ src/knn.py
@@ -13,9 +13,22 @@ class KNN:
         self.dataSet = pd.read_csv(dataFile, header=None)
 
     def run(self):
+        correct = 0
+        incorrect = 0
+
         for index, row in self.trainingSet.iterrows():
             dist, sorted = self.calculateDistances(self.trainingSet.values, row.values)
-            self.getClassification(sorted)
+            classification = self.getClassification(sorted)
+
+            # print('Comparing', classification, ' to ', row.values[5])
+            if classification == row.values[5]:
+                correct += 1
+            else:
+                incorrect += 1
+
+        accuracy = (correct / (correct + incorrect)) * 100
+
+        print('Accuracy is: ', accuracy, '%')
 
     def calculateDistances(self, A, B):
         dist = np.sqrt(((A - B)**2).sum(-1))
@@ -29,7 +42,6 @@ class KNN:
 
         for i in closest:
             key = self.trainingSet.values[i][5]
-            print(key)
             if not key in votes:
                 votes[key] = 1
             else:
